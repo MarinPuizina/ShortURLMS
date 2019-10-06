0a013e1369ade84d5e4d1383937fc14969cfd19a
MarinPuizina <marinpuizina@hotmail.com>
2019-10-07 00:12:10 +0200

    Add Constants class and add constants

--- URLShortenerMS/src/main/java/com/marin/urlshortenerms/service/URLShortenerService.java
+++ URLShortenerMS/src/main/java/com/marin/urlshortenerms/service/URLShortenerService.java
@@ -8,11 +8,12 @@ import redis.clients.jedis.Jedis;
 
 import java.util.Optional;
 
+import static com.marin.urlshortenerms.shared.URLShortenerConstants.ID_TRACKER;
+import static com.marin.urlshortenerms.shared.URLShortenerConstants.SHORT_URL;
+
 @Service
 @Slf4j
 public class URLShortenerService {
-
-    private final String IDTRACKER = "*IDTRACKER*";
 
     @Autowired
     IDConverter idConverter;
@@ -25,25 +26,42 @@ public class URLShortenerService {
         this.jedis = jedis;
     }
 
+    /**
+     * Creates ID Tracker key with value in database.
+     */
     public void createIDTrackerRedis() {
-        if(jedis.get(IDTRACKER) == null || jedis.get(IDTRACKER).isEmpty()) {
-            jedis.set(IDTRACKER, "0");
+        if(jedis.get(ID_TRACKER) == null || jedis.get(ID_TRACKER).isEmpty()) {
+            jedis.set(ID_TRACKER, "0");
         }
     }
 
+    /**
+     * Stores incremented value of the ID Tracker.
+     */
     public void storeIncrementedIDTracker() {
 
         Long idTracker = getStoredIDTracker() + 1L;
 
-        jedis.set(IDTRACKER, String.valueOf(idTracker));
-        log.info("Storing IDTracker on key: {}, with value: {}", IDTRACKER, idTracker);
+        jedis.set(ID_TRACKER, String.valueOf(idTracker));
+        log.info("Storing IDTracker on key: {}, with value: {}", ID_TRACKER, idTracker);
 
     }
 
+    /**
+     * Returns value of the ID Tracker.
+     *
+     * @return Value of the ID Tracker.
+     */
     public Long getStoredIDTracker() {
-        return Long.parseLong(jedis.get(IDTRACKER));
+        return Long.parseLong(jedis.get(ID_TRACKER));
     }
 
+    /**
+     * Stores Unique ID and Source URL in database.
+     *
+     * @param uniqueID The key for storing the sourceURL.
+     * @param sourceURL Original URL which we want to store.
+     */
     public void storeUniqueIDAndSourceURL(String uniqueID, Optional<String> sourceURL) {
 
         //log.info("Storing uniqueID: {} , sourceURL: {}", uniqueID, sourceURL.get());
@@ -54,11 +72,17 @@ public class URLShortenerService {
 
     }
 
+    /**
+     * Creates shorten URL.
+     *
+     * @param uniqueID Used for creating unique short URL.
+     * @return Shorten URL.
+     */
     public String createShortenURL(String uniqueID) {
 
         //log.info("Creating shorten URL: www.shorty.com/{}", uniqueID);
 
-        return "www.shorty.com/" + uniqueID;
+        return SHORT_URL + uniqueID;
     }
 
 }
---
+++ URLShortenerMS/src/main/java/com/marin/urlshortenerms/shared/URLShortenerConstants.java
@@ -0,0 +1,8 @@
+package com.marin.urlshortenerms.shared;
+
+public class URLShortenerConstants {
+
+    public static final String ID_TRACKER = "*ID_TRACKER*";
+    public static final String SHORT_URL = "www.shorty.com/";
+
+}
