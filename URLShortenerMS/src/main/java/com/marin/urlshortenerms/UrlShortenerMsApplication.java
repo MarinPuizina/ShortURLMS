a6ed7567bb0cfc1815371bd26eff88845960b0dc
MarinPuizina <marinpuizina@hotmail.com>
2019-10-06 14:15:55 +0200

    Convert URL Shortener MS to Eureka Client

--- URLShortenerMS/src/main/java/com/marin/urlshortenerms/UrlShortenerMsApplication.java
+++ URLShortenerMS/src/main/java/com/marin/urlshortenerms/UrlShortenerMsApplication.java
@@ -2,10 +2,12 @@ package com.marin.urlshortenerms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UrlShortenerMsApplication {
 
 	public static void main(String[] args) {
