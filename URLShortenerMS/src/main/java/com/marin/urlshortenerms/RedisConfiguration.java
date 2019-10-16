fd4275157d3bff7ceb53edfc8d06037285610127
MarinPuizina <marinpuizina@hotmail.com>
2019-10-16 21:56:15 +0200

    Add properties for redis in URLShortenerMS

--- URLShortenerMS/src/main/java/com/marin/urlshortenerms/RedisConfiguration.java
+++ URLShortenerMS/src/main/java/com/marin/urlshortenerms/RedisConfiguration.java
@@ -1,7 +1,9 @@
 package com.marin.urlshortenerms;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -9,9 +11,17 @@ import org.springframework.data.redis.core.RedisTemplate;
 @Configuration
 public class RedisConfiguration {
 
+    Environment environment;
+
+    @Autowired
+    public RedisConfiguration(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
     JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory(new RedisStandaloneConfiguration("localhost", 6379));
+        return new JedisConnectionFactory(new RedisStandaloneConfiguration(
+                environment.getProperty("redis.config.hostname"), Integer.parseInt(environment.getProperty("redis.config.port"))));
     }
 
     @Bean
